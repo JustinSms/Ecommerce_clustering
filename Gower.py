fe27b01cd8e64c2f4f8a3498827a972af31a8c21
Parmenion333 <justinsams98@yahoo.de>
2020-12-17 15:57:05 +0100

    JUstin_17/12/2020_15:57

--- Gower.py
+++ Gower.py
@@ -1,6 +1,20 @@
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import DistanceMetric
+import pandas_analysis
+import gower
+
+data = pd.read_csv("Ecommerce Customers.csv")
+#print(data.head(15))
+
+data_num = data[["Avg. Session Length","Time on App","Time on Website","Length of Membership","Yearly Amount Spent"]]
+data_cat = pandas_analysis.email_series
+
+data_num["Email"] = data_cat
+
+data_new = data_num
+
+#print(data_new.head(5))
 
 # nominal:
 #   1 = one feature is true and the other feature is not
@@ -14,33 +28,12 @@ from sklearn.neighbors import DistanceMetric
 #   (i - 0.5)/ M   (M --> total number of features of m e.g. different lengths to highway from house 1,2,3,4,5 -> M = 5)
 #   after being scaled ordinal variables will be measured like metric variables
 
-#  
+print(gower.gower_matrix(data_new))
 
-
-
-class Gower_distance:
-
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
-        self.num_list = []
-        self.cat_list = []
-
-
-    def gower_calculation(self):
-        
-        for a in self.dataframe:
-            num_data = self.dataframe._get_numeric_data().columns
-            self.num_list = list(num_data)
-
-        for b in self.dataframe:
-            if b in self.num_list:
-                pass
-            else:
-                self.cat_list.append(b)
 
         
 
-             
+       
 
 
 
