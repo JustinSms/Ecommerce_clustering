fef58ecfefd7394130e1670c0372da12c4a5f84a
maxwebcloud <75029895+maxwebcloud@users.noreply.github.com>
2020-12-23 15:53:13 +0100

    Max_23/12/2020_15:52
    cluster validation notebook

---
+++ Validation.py
@@ -0,0 +1,8 @@
+import numpy as pd
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.cluster import metrics #needed for several index  e.g adjusted Rand index 
+
+
+
