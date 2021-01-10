27e4d1987c6a69de6c33782789c726a6249e08df
Parmenion333 <justinsams98@yahoo.de>
2021-01-10 17:54:01 +0100

    Justin_10/1/2021_17:53
    DBSCAN angefangen

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from sklearn.datasets import make_blobs
 import scipy.cluster.hierarchy as sch
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
 import normalization_file
 from sklearn.neighbors import DistanceMetric
 import pandas_analysis
@@ -14,32 +15,10 @@ import numpy
 import Gower
 #numpy.set_printoptions(threshold=sys.maxsize)
 
-#2import dataset and specification
 
+data = Gower.data_matrix_gower
+#print(data)
 
-data = pd.read_csv("Ecommerce Customers.csv") 
-#print(data.head(5))
-
-data_num = data[["Avg. Session Length","Time on App","Time on Website","Length of Membership","Yearly Amount Spent"]]
-data_cat = data[["Email","Address"]]
-data_num_norm = normalization_file.data_num_norm_normfile
-
-#print(data_cat["Address"].head(20))
-
-
-data_test = {'Name':['Tom', 'nick', 'krish', 'jack'],
-        'Age':[20, 21, 19, 18]}
-
-df = pd.DataFrame(data_test)
-
-
-height = [180, 200, 160, 170]
-height_series = pd.Series(height)
-height_series.name = "height"
-
-df.insert(2, "Height", height)
-
-#print(df)
-
-a = Gower.data_matrix_gower
-print(a)
+dbscan = DBSCAN()
+clusters = dbscan.fit_predict(data)
+print(clusters)
