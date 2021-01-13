99ed7960659bb801d68fefd43ae8eb03d99f70f4
Parmenion333 <justinsams98@yahoo.de>
2021-01-13 16:12:20 +0100

    Justin_13/1/2021_16:12
    PCA

--- test.py
+++ test.py
@@ -16,18 +16,65 @@ import Gower
 from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import SpectralClustering
 from sklearn.cluster import AffinityPropagation
+from numpy.linalg import inv
+from itertools import cycle
+from sklearn import metrics
 
 #numpy.set_printoptions(threshold=sys.maxsize)
-data_matrix = Gower.distance_matrix
+data_matrix = Gower.data_matrix_gower
 
-# SPECTRALCLUSTERING
-sc = SpectralClustering(5, affinity='precomputed', n_init=100,assign_labels='discretize')
-sc.fit_predict(data_matrix)
-
-print(sc.labels_)
-
+## 1 - GM
+data_similarity_matrix = 1 - data_matrix
+#print(data_similarity_matrix)
+data_num_norm = normalization_file.data_num_norm_normfile
 
 
+
+## Affinity Propagation
+"""af = AffinityPropagation().fit(data_similarity_matrix)
+cluster_centers_indices = af.cluster_centers_indices_
+labels = af.labels_
+
+n_clusters_ = len(cluster_centers_indices)"""
+
+## PCA 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+x = StandardScaler().fit_transform(data_num_norm)
+
+pca = PCA(n_components=2)
+
+principal_components = pca.fit_transform(x)
+
+principal_df = pd.DataFrame(data= principal_components, columns= ["principal component 1","principal component 2"])
+
+colors = ["red","green","blue","orange","yellow","black","grey","purple","pink"]
+
+## Visualisation
+"""import matplotlib.pyplot as plt 
+from itertools import cycle 
+  
+plt.close('all') 
+plt.figure(1) 
+plt.clf() 
+  
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk') 
+  
+for k, col in zip(range(n_clusters_), colors): 
+    class_members = labels == k 
+    cluster_center = data_similarity_matrix[cluster_centers_indices[k]] 
+    plt.plot(data_similarity_matrix[class_members, 0], data_similarity_matrix[class_members, 1], col + '.') 
+    plt.plot(cluster_center[0], cluster_center[1], 'o', 
+             markerfacecolor = col, markeredgecolor ='k', 
+             markersize = 14) 
+  
+    for x in data_similarity_matrix[class_members]: 
+        plt.plot([cluster_center[0], x[0]],  
+                 [cluster_center[1], x[1]], col) 
+  
+plt.title('Estimated number of clusters: % d' % n_clusters_) 
+plt.show() """
 
 
 
